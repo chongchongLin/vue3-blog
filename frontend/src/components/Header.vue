8cc60f82307db2913c23ca7552c01c8a2e425463
lin98 <1726928218@qq.com>
2024-12-16 18:03:38 +0800

    chore: 更新Docker CI/CD工作流，添加前端部署步骤并注释Blog链接

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,7 +3,7 @@
     <nav class="navigation">
       <div class="left-nav">
         <router-link to="/" class="nav-item">Home</router-link>
-        <router-link to="/blog" class="nav-item">Blog</router-link>
+        <!-- <router-link to="/blog" class="nav-item">Blog</router-link> -->
         <router-link to="/toolbox" class="nav-item">Toolbox</router-link>
         <router-link to="/resume" class="nav-item">Resume</router-link>
         <router-link to="/about" class="nav-item">About</router-link>
